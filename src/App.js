bfb22b82f6e2db8bd9dcca7ab6ad2daa758edd6d
jyotirs-dev <jyotirsolanki@gmail.com>
2021-05-05 22:44:32 +0530

    api call added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {apiData: null}
+  }
+
+  componentDidMount(){
+    this.getSlotsForDate();
+  }
+
+   getSlotsForDate = (DATE)=> {
+    let config = {
+        method: 'get',
+        url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=319&date=06-05-2021',
+        headers: {
+            'accept': 'application/json'
+        }
+    };
+
+    axios(config)
+        .then(function (slots) {
+            const centers = slots.data.centers;
+            const sessions = centers.map(centre=>centre.sessions).flat();
+            const validSlots = sessions.filter(session=> session.min_age_limit===18 && session.available_capacity>0)
+            console.log("test",validSlots)
+            if(validSlots.length > 0) {
+                alert("Slots Available");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.apiData}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
